13c1ba63fe1e4315085737d4f60b8b85f36aa12b
tandzakhoza <khozatandza@gmail.com>
2021-08-21 09:54:49 +0200

    'demo'

---
+++ Xamarindemo/Xamarindemo/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Xamarindemo.Models
+{
+    public class Item
+    {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Xamarindemo/Xamarindemo/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using Xamarindemo.Views;
+
+namespace Xamarindemo.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string confirm_password { get; set; }
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
